d660e0d758d0b09792ee85b88de0480a67c10d94
Slugen <umargatiatullin14@gmail.com>
2024-03-07 10:00:05 +0300

    Addedd output with two languages

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Спокойной ночи мир");
+        System.out.println("Спокойной ночи мир");
+        System.out.println("Good night world");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
